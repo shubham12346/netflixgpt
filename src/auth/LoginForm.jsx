4a2e68b75b2f79e4a8e89cee4fcc596bfb866b5b
shubham agrahari <aakashagrahari26@gmail.com>
2024-08-18 00:06:59 +0530

    Added loging componet

--- src/auth/LoginForm.jsx
+++ src/auth/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
+import "./login.css";
 
 const LoginForm = () => {
   const { t } = useTranslation("login");
@@ -10,41 +11,80 @@ const LoginForm = () => {
   const email = useRef(null);
 
   const handleSubmit = (e) => {
+    e.preventDefault();
     setIsSignUpForm(true);
+  };
+
+  const handleFormChange = () => {
+    setIsSignUpForm((prev) => !prev);
   };
 
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
-        <div className="h-80 flex flex-col p-5">
-          <div className="text-white">
-            {isSignUpForm ? t("singUp") : t("singIn")}
+        <div className="md:h-[75vh] flex flex-col p-5 text-white">
+          <div className="text-white py-4">
+            {isSignUpForm ? t("signUp") : t("signIn")}
           </div>
-          {isSignUpForm && (
+
+          <div className={`mx-2 ${isSignUpForm ? "" : "hidden"}`}>
+            <label htmlFor="username">{t("username")}</label>
             <input
               type="text"
-              name="email"
-              className="p-2 mx-2 my-2"
+              name="username"
+              className="p-2 my-2 bg-black opacity-70 border-2 border-white  w-full"
+              placeholder="Enter your name"
               ref={name}
+              autoComplete="off"
+              autoFocus
             />
-          )}
-          <input
-            type="email"
-            name="email"
-            className="p-2 mx-2 my-2"
-            ref={email}
-          />
-          <input
-            type="password"
-            name="password"
-            id=""
-            className="p-2 mx-2 my-2"
-            ref={password}
-          />
+          </div>
+          <div className="mx-2">
+            <label htmlFor="email">{t("email")}</label>
+            <input
+              type="email"
+              name="email"
+              className="p-2 my-2 bg-black opacity-70 border-2  w-full"
+              placeholder="Enter your email "
+              ref={email}
+              autoComplete="off"
+              autoFocus="on"
+            />
+          </div>
+          <div className="mx-2 mb-6">
+            <label htmlFor="password">{t("password")}</label>
 
-          <button className="bg-red-600 text-white p-2 mx-2 my-2">
-            {isSignUpForm ? t("singUp") : t("singIn")}
+            <input
+              type="password"
+              name="password"
+              id=""
+              className="p-2  my-2  bg-black opacity-70border-2 border-white  w-full"
+              ref={password}
+              placeholder="Enter your password "
+              autoComplete="off"
+              autoFocus="on"
+            />
+          </div>
+
+          <button className="bg-red-600  p-2 mx-2 my-2 mb-8">
+            {isSignUpForm ? t("signUp") : t("signIn")}
           </button>
+
+          <div
+            className="text-white cursor-pointer mx-2 mb-20"
+            onClick={handleFormChange}
+          >
+            {isSignUpForm ? (
+              <h5>
+                {t("alreadyHaveAnAccount")} <span> {t("signIn")}</span>
+              </h5>
+            ) : (
+              <h5>
+                {t("newLogin")}
+                <span className=""> {t("signUp")}</span>
+              </h5>
+            )}
+          </div>
         </div>
       </form>
     </div>
